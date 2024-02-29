5e265c8ddd5c81c0cbfeb46a9b9af44d21428207
RaphaCBa9 <raphaelb3@al.insper.edu.br>
2024-02-29 12:27:17 +0000

    ta chave

--- main/main.c
+++ main/main.c
@@ -8,6 +8,8 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+const int ON_OFF = 28;
+
 const int LED_Y = 14;
 const int BTN_Y = 15;
 
@@ -22,12 +24,35 @@ const int BTN_R = 17;
 
 const int BUZZER = 7;
 
+
+volatile int BTN_ON_OFF = 0;
 volatile char BTN_Y_FLAG = 0;
 volatile char BTN_B_FLAG = 0;
 volatile char BTN_G_FLAG = 0;
 volatile char BTN_R_FLAG = 0;
+volatile int start_flag = 0;
+
+void playtone(int freq, int duration) {
+    int period = 1000000 / freq;
+    int cycles = duration * freq / 1000;
+    for (int i = 0; i < cycles; i++) {
+        gpio_put(BUZZER, 1);
+        sleep_us(period / 2);
+        gpio_put(BUZZER, 0);
+        sleep_us(period / 2);
+    }
+}
+
+void start(void){
+    playtone(1000, 50);
+    sleep_ms(50);
+}
 
 void btn_callback(uint gpio, uint32_t events) {
+    if (gpio == ON_OFF){
+        BTN_ON_OFF = !BTN_ON_OFF;
+
+    }
     if (gpio == BTN_Y) {
         BTN_Y_FLAG = 1;
     } else if (gpio == BTN_B) {
@@ -43,6 +68,7 @@ void btn_callback(uint gpio, uint32_t events) {
 int main() {
     stdio_init_all();
     gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
     gpio_init(LED_Y);
     gpio_init(LED_B);
     gpio_init(LED_G);
@@ -51,6 +77,8 @@ int main() {
     gpio_init(BTN_B);
     gpio_init(BTN_G);
     gpio_init(BTN_R);
+    gpio_init(ON_OFF);
+    gpio_set_dir(ON_OFF, GPIO_IN);
     gpio_set_dir(LED_Y, GPIO_OUT);
     gpio_set_dir(LED_B, GPIO_OUT);
     gpio_set_dir(LED_G, GPIO_OUT);
@@ -63,35 +91,48 @@ int main() {
     gpio_pull_up(BTN_B);
     gpio_pull_up(BTN_G);
     gpio_pull_up(BTN_R);
+    gpio_pull_up(ON_OFF);
     gpio_set_irq_enabled_with_callback(BTN_Y, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
     gpio_set_irq_enabled(BTN_B, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(BTN_G, GPIO_IRQ_EDGE_FALL, true);
     gpio_set_irq_enabled(BTN_R, GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(ON_OFF, GPIO_IRQ_EDGE_FALL, true);
     while (true) {
+        printf("%d\n", BTN_ON_OFF);
+        if (BTN_ON_OFF){
 
-        if(BTN_Y_FLAG){
-            BTN_Y_FLAG = 0;
-            gpio_put(LED_Y, 1);
-            sleep_ms(500);
-            gpio_put(LED_Y, 0);
-        }
-        if(BTN_B_FLAG){
-            BTN_B_FLAG = 0;
-            gpio_put(LED_B, 1);
-            sleep_ms(500);
-            gpio_put(LED_B, 0);
-        }
-        if(BTN_G_FLAG){
-            BTN_G_FLAG = 0;
-            gpio_put(LED_G, 1);
-            sleep_ms(500);
-            gpio_put(LED_G, 0);
-        }
-        if(BTN_R_FLAG){
-            BTN_R_FLAG = 0;
-            gpio_put(LED_R, 1);
-            sleep_ms(500);
-            gpio_put(LED_R, 0);
+            if(BTN_Y_FLAG){
+                BTN_Y_FLAG = 0;
+                gpio_put(LED_Y, 1);
+                playtone(1000, 100);
+                sleep_ms(50);
+                
+                gpio_put(LED_Y, 0);
+            }
+            if(BTN_B_FLAG){
+                BTN_B_FLAG = 0;
+                gpio_put(LED_B, 1);
+                playtone(1100, 100);
+                sleep_ms(50);
+                
+                gpio_put(LED_B, 0);
+            }
+            if(BTN_G_FLAG){
+                BTN_G_FLAG = 0;
+                gpio_put(LED_G, 1);
+                playtone(1200, 100);
+                sleep_ms(50);
+                
+                gpio_put(LED_G, 0);
+            }
+            if(BTN_R_FLAG){
+                BTN_R_FLAG = 0;
+                gpio_put(LED_R, 1);
+                playtone(1300, 100);
+                sleep_ms(50);
+                
+                gpio_put(LED_R, 0);
+            }
         }
     }
 }
